bd65718fea74bd0de44a3b7a2daed8a4935745ef
zubairmoerat <zubairmoeratofficial@gmail.com>
2023-11-20 14:58:59 +0200

    javaAdded

--- script/code.js
+++ script/code.js
@@ -1,3 +1,25 @@
-//input
-//function
-//output
+let personName = document.querySelector('[data-person]')
+let birthdateInput = document.getElementById('birthdate').value
+
+
+
+function Birthday(){
+    let personName = document.querySelector('[data-person]')
+    let birthdateInput = document.getElementById('birthdate').value
+    const calculate = document.getElementById('calculate')   
+    let result = document.querySelector('[data-output]')
+
+    const birthdate = new Date(birthdateInput);
+    const currentDate = new Date();
+
+    birthdate.setFullYear(currentDate.getFullYear());
+    if(birthdate < currentDate){
+        birthdate.setFullYear(currentDate.getFullYear()+1)
+    }
+    const timeDifference = birthdate.getTime() - currentDate.getTime();
+    const daysUntilBirthday = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+}
+calculate.addEventListener('click',Birthday)
+document.querySelector('[data-output]').textContent = `Hello ${personName}, There are ${daysUntilBirthday} days until your birthday !!`
+
+//const numbOfDays = difference / (1000 * 3600 * 24)
